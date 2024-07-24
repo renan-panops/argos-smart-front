9d28297b31fe17481248d99fa64529450b849896
Renan Brayner <renan@panops.com.br>
2024-07-24 11:58:34 -0300

    fix: corrige endpoint da api

--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -14,7 +14,7 @@ declare module '@vue/runtime-core' {
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-const api = axios.create({ baseURL: 'http://127.0.0.1:8000/api' });
+const api = axios.create({ baseURL: 'http://localhost:8001/api' });
 
 export default boot(({ app }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
